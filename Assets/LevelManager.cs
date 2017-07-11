56bfc5fcffae923e95ae157a4804c2e5b3d1ee56
Ryan Morgan <Morganglassworks@gmail.com>
2017-07-10 18:10:47 -0700

    Add files via upload

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level Load Requested for: " + name);
+		Application.LoadLevel(name); 
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Player would like to quit");
+		Application.Quit();
+	}
+
+}
